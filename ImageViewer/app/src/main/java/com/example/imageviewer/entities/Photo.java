ba66f0d4df3342a3056bff187d752d2da73aebb3
SergSerg <sergio.ne@inbox.ru>
2020-08-13 19:34:26 +0600

    Создал класс Photo.

--- ImageViewer/app/src/main/java/com/example/imageviewer/entities/Photo.java
+++ ImageViewer/app/src/main/java/com/example/imageviewer/entities/Photo.java
@@ -1,4 +1,17 @@
 package com.example.imageviewer.entities;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class Photo {
+    @Expose
+    @SerializedName("totalHits")
+    public int totalHits;
+
+    @Expose
+    @SerializedName("hits")
+    public List<Hit> hits;
+
 }
